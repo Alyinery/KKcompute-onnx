fcad61ff210047b44f46ca0c059be0f2a844b696
jfdu <55056593+dududjf@users.noreply.github.com>
2025-08-05 20:05:42 +0800

    Add the test file for the first operator.

---
+++ src/kp_onnx_test/kop_erf_test.py
@@ -0,0 +1,25 @@
+from kp import Manager
+import numpy as np
+import time
+from math import erf
+from kp_onnx.kop_erf import ErfOp
+
+device_id = 0
+mgr = Manager(device_id)
+print(mgr.list_devices()[device_id])
+
+erf_op = ErfOp(mgr, ['input'], ['output'])
+vec_erf_func = np.vectorize(erf)
+numpy_in = np.random.random(1024 * 1024)
+
+start_time = time.time()
+numpy_out = vec_erf_func(numpy_in)
+print("Numpy:", time.time() - start_time, "seconds")
+
+start_time = time.time()
+kp_out = erf_op.run(numpy_in)[0]
+print(f"{erf_op}:", time.time() - start_time, "seconds")
+
+print(numpy_out)
+print(kp_out)
+print(np.allclose(numpy_out, kp_out, rtol=1e-4, atol=1e-4))
