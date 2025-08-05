5a76346b7f9a11fbd2deeb43f0462fcc9e3937ee
jfdu <55056593+dududjf@users.noreply.github.com>
2025-08-05 20:30:20 +0800

    Update kop_erf.py

--- src/kp_onnx/kop_erf.py
+++ src/kp_onnx/kop_erf.py
@@ -6,8 +6,8 @@ from pyshader.stdlib import exp, sign, abs
 
 @python2shader
 def compute_shader_erf(index=("input", "GlobalInvocationId", ivec2),
-                       data1=("buffer", 0, Array(f32)),
-                       data2=("buffer", 1, Array(f32))):
+                       in_data=("buffer", 0, Array(f32)),
+                       out_data=("buffer", 1, Array(f32))):
     i = index.x
     a1 = 0.254829592
     a2 = -0.284496736
@@ -15,12 +15,12 @@ def compute_shader_erf(index=("input", "GlobalInvocationId", ivec2),
     a4 = -1.453152027
     a5 = 1.061405429
     p = 0.3275911
-    x = data1[i]
+    x = in_data[i]
     s = sign(x)
     x_abs = abs(x)
     t = 1.0 / (1.0 + p * x_abs)
     y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * exp(-x_abs * x_abs)
-    data2[i] = s * y
+    out_data[i] = s * y
 
 
 _erf_code = compute_shader_erf.to_spirv()
@@ -57,4 +57,5 @@ class ErfOp:
         tensor_in.destroy()
         tensor_out.destroy()
         return outputs
+
 
