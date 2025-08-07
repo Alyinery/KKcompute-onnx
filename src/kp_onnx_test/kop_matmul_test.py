ce52007777d89931881b8f4b52d8c586efd94899
jfdu <55056593+dududjf@users.noreply.github.com>
2025-08-07 18:01:11 +0800

    Update kop_matmul_test.py for two cases

--- src/kp_onnx_test/kop_matmul_test.py
+++ src/kp_onnx_test/kop_matmul_test.py
@@ -8,18 +8,34 @@ mgr = Manager(device_id)
 print(mgr.list_devices()[device_id])
 
 matmul_op = MatMulOp(mgr, ['input1', 'input2'], ['output'])
-numpy_in_1 = np.random.random((5, 1000, 2000))
-numpy_in_2 = np.random.random((2000, 500))
+numpy_in_1 = np.random.random((2, 5, 1000, 512))
+numpy_in_2 = np.random.random((512, 1024))
 
 start_time = time.time()
 numpy_out = np.matmul(numpy_in_1, numpy_in_2)
 print("Numpy:", time.time() - start_time, "seconds")
 
-# matmul_op.run(numpy_in_1, numpy_in_2)  # warm up
 start_time = time.time()
 kp_out = matmul_op.run(numpy_in_1, numpy_in_2)[0]
 print(f"{matmul_op}:", time.time() - start_time, "seconds")
 
-print(numpy_out)
-print(kp_out)
+# print(numpy_out)
+# print(kp_out)
+print('Max error:', np.abs(numpy_out - kp_out).max())
+print(np.allclose(numpy_out, kp_out, rtol=1e-4, atol=1e-4))
+
+numpy_in_1 = np.random.random((2, 5, 1000, 512))
+numpy_in_2 = np.random.random((2, 5, 512, 1024))
+
+start_time = time.time()
+numpy_out = np.matmul(numpy_in_1, numpy_in_2)
+print("Numpy:", time.time() - start_time, "seconds")
+
+start_time = time.time()
+kp_out = matmul_op.run(numpy_in_1, numpy_in_2)[0]
+print(f"{matmul_op}:", time.time() - start_time, "seconds")
+
+# print(numpy_out)
+# print(kp_out)
+print('Max error:', np.abs(numpy_out - kp_out).max())
 print(np.allclose(numpy_out, kp_out, rtol=1e-4, atol=1e-4))
